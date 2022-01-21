6086fa60787224dc708ebd0210ea25a5f86a310c
Ivan <petyk98@gmail.com>
2022-01-21 20:09:13 +0700

    +upgrade columns in db +add and upgrade funcs for show data from db +changed data type of db columns

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 from flask import Flask, render_template, url_for, request, redirect
 from flask_sqlalchemy import SQLAlchemy
-from datetime import datetime
-
+from datetime import datetime, time
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///sleepdairy.db'
@@ -18,40 +17,62 @@ class Notation(db.Model):
     usnul = db.Column(db.Time, nullable=False)
     prosnul = db.Column(db.Time, nullable=False)
     vstal = db.Column(db.Time, nullable=False)
-    nespal = db.Column(db.Time, nullable=False)
+    nespal = db.Column(db.INT, nullable=False)
+    spal = db.Column(db.INT, nullable=False)
+    vkrovati = db.Column(db.INT, nullable=False)
 
-    # def __repr__(self):
-    #     return '<Notation %r>' % self.id
-
-
-def strtotime(self):
-    return datetime.time(datetime.strptime(self, "%H:%M"))
+    def __repr__(self):
+        return '<Notation %r>' % self.id
 
 
-def strtoymd(self):
-    return datetime.date(datetime.strptime(self, "%Y-%m-%d"))
+def str_to_time(self):
+    return datetime.time(datetime.strptime(self, '%H:%M'))
+
+
+def str_to_ymd(self):
+    return datetime.date(datetime.strptime(self, '%Y-%m-%d'))
+
+
+def timedelta_to_minutes(self):
+    return self.seconds / 60
 
 
 @app.route('/sleep')
 def sleep():
+    def h_m(self):
+        if type(self) == int:
+            hm = str(self // 60) + ':' + str(self % 60)
+        else:
+            hm = self.strftime('%H:%M')
+        return hm
+
+    def eff(spal, nespal, vkrovati):
+        return round(((spal - nespal) / vkrovati * 100), 2)
+
     notations = Notation.query.order_by(Notation.date.desc()).all()
-    return render_template("sleep.html", notations=notations)
+    return render_template("sleep.html", notations=notations, h_m=h_m, eff=eff)
 
 
 @app.route('/sleep', methods=['POST', 'GET'])
-def addnotation():
+def add_notation():
     if request.method == "POST":
-        date = strtoymd(request.form['date'])
+        date = str_to_ymd(request.form['date'])
         week = request.form['week']
         day = request.form['day']
-        leg = strtotime(request.form['leg'])
-        usnul = strtotime(request.form['usnul'])
-        prosnul = strtotime(request.form['prosnul'])
-        vstal = strtotime(request.form['vstal'])
-        nespal = strtotime(request.form['nespal'])
+        leg = str_to_time(request.form['leg'])
+        usnul = str_to_time(request.form['usnul'])
+        prosnul = str_to_time(request.form['prosnul'])
+        vstal = str_to_time(request.form['vstal'])
+        nespal = str_to_time(request.form['nespal']).hour * 60 + str_to_time(request.form['nespal']).minute
 
-        notation = Notation(date=date, week=week, day=day, leg=leg, usnul=usnul, prosnul=prosnul, vstal=vstal,
-                            nespal=nespal)
+        delta_spal = datetime.combine(date, prosnul) - datetime.combine(date, usnul)
+        delta_vkrovati = datetime.combine(date, vstal) - datetime.combine(date, leg)
+
+        spal = timedelta_to_minutes(delta_spal) - nespal
+        vkrovati = timedelta_to_minutes(delta_vkrovati)
+
+        notation = Notation(date=date, week=week, day=day, spal=spal, vkrovati=vkrovati, leg=leg, usnul=usnul,
+                            prosnul=prosnul, vstal=vstal, nespal=nespal)
         try:
             db.session.add(notation)
             db.session.commit()
@@ -60,6 +81,7 @@ def addnotation():
             return "При добавлении статьи произошла ошибка"
     else:
         return render_template('sleep.html')
+
 
 @app.route('/')
 @app.route('/main')
