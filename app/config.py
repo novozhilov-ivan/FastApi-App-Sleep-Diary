30555ab621af8f85f3c7752deb008d74c26c59a5
Ivan <petyk98@gmail.com>
2022-03-30 20:52:34 +0700

    deploy 4

--- app/config.py
+++ app/config.py
@@ -9,11 +9,12 @@ app = Flask(__name__, template_folder='templates', static_folder="static")
 app.secret_key = 'super secret key'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///sleep_diary.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['MAX_CONTENT_LENGTH'] = 1024 * 1024
 APP_MODE = os.getenv('FLASK_ENV')
 
 # Создание бд: from app import db; db.create_all()
 db = SQLAlchemy(app)
-MAX_CONTENT_LENGTH = 1024 * 1024
+
 login_manager = LoginManager(app)
 login_manager.login_view = 'sign_in'
 login_manager.login_message = "Дневник сна доступен только авторизованным пользователям"
