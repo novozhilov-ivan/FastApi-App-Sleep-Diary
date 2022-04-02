59987518bf8aec95433f940ae0fc939d6a7d7a0b
Ivan <petyk98@gmail.com>
2022-04-02 17:58:49 +0700

    heroku deploy

--- app/config.py
+++ app/config.py
@@ -1,21 +1,19 @@
 import os
 
 from dotenv import load_dotenv
-
 from flask import Flask
 from flask_login import LoginManager
 from flask_sqlalchemy import SQLAlchemy
 
-# todo попробовать взять и задействовать from psycopg2 import connect
-
 load_dotenv()
-DATABASE_URI='postgresql://lqdyhlvwlirokb:d2aa84740d77846e6b1aa25745a2e2c7db22cba92a2b69cb815a4c9444639d60@ec2-52-30-67-143.eu-west-1.compute.amazonaws.com:5432/d2l2sup15h6ek1'
 
 app = Flask('Sleep diary', template_folder='app/templates', static_folder='app/static')
+
 # Для создания локальной бд
 # app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///sleep_diary.db'
 # Для пользования или для серверной бд
-app.config['SQLALCHEMY_DATABASE_URI'] = DATABASE_URI
+app.config['SQLALCHEMY_DATABASE_URI'] = f"postgresql://{os.getenv('DB_USER')}:{os.getenv('DB_PASSWORD')}@" \
+                                        f"{os.getenv('DB_HOST')}:{os.getenv('DB_PORT')}/{os.getenv('DB_NAME')}"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
 
