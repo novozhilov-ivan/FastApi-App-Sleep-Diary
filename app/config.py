a20ef606357f32a2e719504ea10e889f637f9c51
Ivan <petyk98@gmail.com>
2022-04-02 16:41:45 +0700

    2 set settings for database. heroku deploy

--- app/config.py
+++ app/config.py
@@ -8,11 +8,9 @@ from flask_sqlalchemy import SQLAlchemy
 
 # todo попробовать взять и задействовать from psycopg2 import connect
 
-
 load_dotenv()
 
 app = Flask('Sleep diary', template_folder='app/templates', static_folder='app/static')
-
 # Для создания локальной бд
 # app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///sleep_diary.db'
 # Для пользования или для серверной бд
@@ -20,9 +18,9 @@ app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URI')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
 
+
 # Создание бд: from app import db; db.create_all()
 db = SQLAlchemy(app)
-
 
 login_manager = LoginManager(app)
 login_manager.login_view = 'sign_in'
