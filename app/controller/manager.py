af66743d3de41f276e4735a5f1c64981118997c0
Ivan <petyk98@gmail.com>
2022-04-08 23:06:59 +0700

    add @property to Manager

--- app/controller/manager.py
+++ app/controller/manager.py
@@ -1,57 +1,62 @@
 from datetime import datetime, time, date, timedelta
 
 from flask import render_template, request, redirect, url_for, flash
+from flask_login import login_required, current_user
 
 from app.controller import *
+from app.model import *
 
-
-# notation = Notation.add_notation_and_commit(
-#     request.form.form['calendar_date'],
-#     request.form['bedtime'],
-#     request.form['asleep'],
-#     request.form['awake'],
-#     request.form['rise'],
-#     request.form['without_sleep']
-# )
 
 class DiaryEntryManager:
     def __init__(self, calendar_date=None, bedtime=None, asleep=None, awake=None, rise=None,
                  without_sleep=None, sleep_duration=None, time_in_bed=None):
         self.calendar_date = calendar_date
-        self.bedtime = bedtime
-        self.asleep = asleep
-        self.awake = awake
-        self.rise = rise
-        self.without_sleep = without_sleep
-        self.sleep_duration = sleep_duration
-        self.time_in_bed = time_in_bed
+        self._bedtime = bedtime
+        self._asleep = asleep
+        self._awake = awake
+        self._rise = rise
+        self._without_sleep = without_sleep
+        self._sleep_duration = sleep_duration
+        self._time_in_bed = time_in_bed
 
         if self.calendar_date is not None:
-            sleep_time_check(self.calendar_date, self.bedtime, self.asleep)
-            wake_up_time_check(self.calendar_date, self.awake, self.rise)
-            sleep_duration_less_time_in_bed(
-                self.calendar_date, self.bedtime, self.asleep, self.awake, self.rise
-            )
-        print(f"\n\nЗаготовка для записи: "
-              f"[Дата: {self.calendar_date}, Лег: {self.bedtime},"
-              f" Уснул: {self.asleep}, Проснулся: {self.awake},"
-              f" Встал: {self.rise}, Не спал: {self.without_sleep}]\n\n")
+            sleep_time_check(self.calendar_date, self._bedtime, self._asleep)
+            wake_up_time_check(self.calendar_date, self._awake, self._rise)
+            sleep_duration_less_time_in_bed(self.calendar_date, self._bedtime, self._asleep, self._awake, self._rise)
 
     def __repr__(self):
-        return f"\n\nЗаготовка для записи: " \
-               f"[Дата: {self.calendar_date}, Лег: {self.bedtime}," \
-               f" Уснул: {self.asleep}, Проснулся: {self.awake}," \
-               f" Встал: {self.rise}, Не спал: {self.without_sleep}]\n\n"
+        return f"\n\nЗаготовка для записи: [Дата: {self.calendar_date}, Лег: {self._bedtime}, Уснул: {self._asleep}," \
+               f" Проснулся: {self._awake}, Встал: {self._rise}, Не спал: {self._without_sleep}]\n\n"
 
-    def get_forms_data_and_reformat(self):
+    # это получился сеттер но тут будет геттер
+    @property
+    def diary_entry(self):
         self.calendar_date = datetime.strptime(request.form['calendar_date'], "%Y-%m-%d")
-        self.bedtime = str_to_time(request.form['bedtime'][:5])
-        self.asleep = str_to_time(request.form['asleep'][:5])
-        self.awake = str_to_time(request.form['awake'][:5])
-        self.rise = str_to_time(request.form['rise'][:5])
-        self.without_sleep = str_to_time(request.form['without_sleep'][:5])
-        return DiaryEntryManager(
-            self.calendar_date, self.bedtime, self.asleep, self.awake, self.rise, self.without_sleep
+        self._bedtime = str_to_time(request.form['bedtime'][:5])
+        self._asleep = str_to_time(request.form['asleep'][:5])
+        self._awake = str_to_time(request.form['awake'][:5])
+        self._rise = str_to_time(request.form['rise'][:5])
+        self._without_sleep = str_to_time(request.form['without_sleep'][:5])
+        # плюс рассчитанные значения из верхних
+        return Notation(
+            calendar_date=self.calendar_date, bedtime=self._bedtime, asleep=self._asleep,
+            awake=self._awake, rise=self._rise, without_sleep=self._without_sleep,
+            user_id=current_user.id
+        )
+    # в целом не то что нужно
+    @diary_entry.setter
+    def diary_entry(self):
+        self.calendar_date = datetime.strptime(request.form['calendar_date'], "%Y-%m-%d")
+        self._bedtime = str_to_time(request.form['bedtime'][:5])
+        self._asleep = str_to_time(request.form['asleep'][:5])
+        self._awake = str_to_time(request.form['awake'][:5])
+        self._rise = str_to_time(request.form['rise'][:5])
+        self._without_sleep = str_to_time(request.form['without_sleep'][:5])
+        # плюс рассчитанные значения из верхних
+        Notation(
+            calendar_date=self.calendar_date, bedtime=self._bedtime, asleep=self._asleep,
+            awake=self._awake, rise=self._rise, without_sleep=self._without_sleep,
+            user_id=current_user.id
         )
 
     @staticmethod
--- app/view_functions/sleep_diary.py
+++ app/view_functions/sleep_diary.py
@@ -9,36 +9,36 @@ from app.model import *
 
 @login_required
 def create_and_save_entry():
-    try:
-        # calendar_date = request.form['calendar_date']
-        # bedtime = request.form['bedtime']
-        # asleep = request.form['asleep']
-        # awake = request.form['awake']
-        # rise = request.form['rise']
-        # without_sleep = request.form['without_sleep']
-        # without_sleep = without_sleep.hour * 60 + without_sleep.minute
-        # sleep_duration = get_timedelta(calendar_date, awake, asleep).seconds / 60 - without_sleep
-        # time_in_bed = get_timedelta(calendar_date, rise, bedtime).seconds / 60
-        new_entry = DiaryEntryManager().get_forms_data_and_reformat()
+    # try:
+    # calendar_date = request.form['calendar_date']
+    # bedtime = request.form['bedtime']
+    # asleep = request.form['asleep']
+    # awake = request.form['awake']
+    # rise = request.form['rise']
+    # without_sleep = request.form['without_sleep']
+    # without_sleep = without_sleep.hour * 60 + without_sleep.minute
+    # sleep_duration = get_timedelta(calendar_date, awake, asleep).seconds / 60 - without_sleep
+    # time_in_bed = get_timedelta(calendar_date, rise, bedtime).seconds / 60
+    # new_entry = DiaryEntryManager().get_forms_data_and_reformat()
 
-        notation = Notation(
-            calendar_date=new_entry.calendar_date, bedtime=new_entry.bedtime, asleep=new_entry.asleep,
-            awake=new_entry.awake, rise=new_entry.rise, without_sleep=new_entry.without_sleep,
-            user_id=current_user.id
-        )
-
-        add_and_commit(notation)
-        flash('Новая запись добавлена в дневник сна')
+    # notation = Notation(
+    #     calendar_date=new_entry.calendar_date, bedtime=new_entry.bedtime, asleep=new_entry.asleep,
+    #     awake=new_entry.awake, rise=new_entry.rise, without_sleep=new_entry.without_sleep,
+    #     user_id=current_user.id
+    # )
+    new_entry = DiaryEntryManager()
+    add_and_commit(new_entry.diary_entry)
+    flash('Новая запись добавлена в дневник сна')
     # except sqlalchemy.exc.IntegrityError:
     #     flash(f'Запись с датой "{notation.calendar_date}" уже существует.')
-    except TimeInBedLessSleepError:
-        flash('Время проведенное в кровати не может быть меньше времени сна.')
-    except ValueError as err:
-        flash(f"{err.args[0]}")
-    except Exception as err:
-        flash(f'При добавлении записи произошла ошибка. Прочая ошибка. {err.args[0]}')
-    finally:
-        return redirect(url_for('get_main_page'))
+    # except TimeInBedLessSleepError:
+    #     flash('Время проведенное в кровати не может быть меньше времени сна.')
+    # except ValueError as err:
+    #     flash(f"{err.args[0]}")
+    # except Exception as err:
+    #     flash(f'При добавлении записи произошла ошибка. Прочая ошибка. {err.args[0]}')
+    # finally:
+    return redirect(url_for('get_main_page'))
     # return redirect(url_for('get_sleep_diary_entries'))
 
 
@@ -47,6 +47,7 @@ def render_sleep_diary_page():
     """Отображает все записи дневника сна из БД"""
     # sleep_diary = Manager.get_sleep_diary()
     all_notations_of_user = get_all_notations_of_user()
+
     return render_template(
         "sleep.html", time_display=time_display, all_notations=all_notations_of_user,
         average_sleep_duration_per_week=get_average_sleep_duration_per_week,
