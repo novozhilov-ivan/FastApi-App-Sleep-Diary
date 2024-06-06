07d96583dd76e7fb414c7ce10f78a8aa6e126732
[Ivan] <petyk98@gmail.com>
2024-06-06 07:16:03 +0700

    Рефакторинг тестов и фикстур. Перенес тесты из ветки api_fix_scopes_test; Перенес pytest.ini.

--- tests/test_api/test_edit/test_export.py
+++ tests/test_api/test_edit/test_export.py
@@ -1,20 +1,25 @@
 import pytest
 from flask import url_for
 from flask.testing import FlaskClient
+from werkzeug.datastructures import Authorization
 
+from api.extension import bearer
+from api.models import User
 from api.routes.edit import response_not_found_404
 from api.routes.edit.export_file import export_notes_endpoint
+from api.utils.jwt import create_access_jwt
 from api.utils.manage_notes import FileDataConverter
 from common.baseclasses.response import Response
 from common.baseclasses.status_codes import HTTP
 from common.generators.diary import SleepDiaryGenerator
 from common.pydantic_schemas.sleep.notes import SleepNote
+from common.pydantic_schemas.user import UserValidate
 
 
-@pytest.mark.edit
-@pytest.mark.export
+@pytest.mark.edit_diary
+@pytest.mark.edit_export
 class TestEditExportNotes:
-    @pytest.mark.export_200
+    @pytest.mark.edit_export_200
     @pytest.mark.parametrize(
         "generated_diary",
         (7, 10),
@@ -23,54 +28,55 @@ class TestEditExportNotes:
     def test_export_notes_200(
         self,
         client: FlaskClient,
-        exist_user_id: int,
+        jwt_access: Authorization,
         saved_diary: SleepDiaryGenerator,
         generated_diary: SleepDiaryGenerator,
     ):
-        query = {"id": exist_user_id}
         response = client.get(
-            url_for(
-                export_notes_endpoint,
-                **query,
-            )
+            path=url_for(
+                endpoint=export_notes_endpoint,
+            ),
+            auth=jwt_access,
         )
-        str_file = FileDataConverter(saved_diary.notes).to_csv_str()
+        converter: FileDataConverter = FileDataConverter(data=saved_diary.notes)
         response = Response(response)
         response.assert_status_code(HTTP.OK_200)
         response.validate(SleepNote)
-        response.assert_data(str_file)
+        response.assert_data(converter.to_csv_str())
 
-    @pytest.mark.export_404
-    @pytest.mark.export_404_user
+    @pytest.mark.edit_export_404
     def test_export_notes_404_user_dont_exist(
         self,
         client: FlaskClient,
+        exist_user: User,
     ):
-        dont_exist_user_id = 999
-        query = {"id": dont_exist_user_id}
+        user = UserValidate.model_validate(exist_user)
+        user.id = 999
+        jwt_with_non_exist_user_id = Authorization(
+            auth_type=bearer,
+            token=create_access_jwt(user),
+        )
         response = client.get(
-            url_for(
-                export_notes_endpoint,
-                **query,
-            )
+            path=url_for(
+                endpoint=export_notes_endpoint,
+            ),
+            auth=jwt_with_non_exist_user_id,
         )
         response = Response(response)
         response.assert_status_code(HTTP.NOT_FOUND_404)
         response.assert_data(response_not_found_404)
 
-    @pytest.mark.export_404
-    @pytest.mark.export_404_notes
+    @pytest.mark.edit_export_404
     def test_export_notes_404_notes_dont_exist(
         self,
         client: FlaskClient,
-        exist_user_id: int,
+        jwt_access: Authorization,
     ):
-        query = {"id": exist_user_id}
         response = client.get(
-            url_for(
-                export_notes_endpoint,
-                **query,
-            )
+            path=url_for(
+                endpoint=export_notes_endpoint,
+            ),
+            auth=jwt_access,
         )
         response = Response(response)
         response.assert_status_code(HTTP.NOT_FOUND_404)
