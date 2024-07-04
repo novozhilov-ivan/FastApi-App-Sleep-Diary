bc2ca0ae73dd1a275247b3e3e540896434c64c72
[Ivan] <petyk98@gmail.com>
2024-07-04 08:53:32 +0700

    Добавил computed_field - weekly_notes_count в WeekStatistic class.

--- src/domain/week/base.py
+++ src/domain/week/base.py
@@ -1,15 +1,21 @@
 import abc
 
 from datetime import time, timedelta
+from typing import Annotated, Callable, Type, TypeAlias
+
 from typing_extensions import Self
 
 from pydantic import (
     BaseModel,
     Field,
     computed_field,
+    conint,
 )
 
 from src.domain import note as nt
+
+
+int_weekly_notes_count = Annotated[int, conint(ge=1, le=7)]
 
 
 class WeekBase(BaseModel, abc.ABC):
@@ -52,6 +58,13 @@ class WeekDurationsBase(WeekBase, abc.ABC):
 
 class WeekStatisticBase(WeekDurationsBase, abc.ABC):
     @computed_field(  # type: ignore[misc]
+        title="Количество записей сна за неделю",
+    )
+    @property
+    @abc.abstractmethod
+    def weekly_notes_count(self: Self) -> int_weekly_notes_count: ...
+
+    @computed_field(  # type: ignore[misc]
         title="Средняя время сна за неделю",
     )
     @property
@@ -64,6 +77,13 @@ class WeekStatisticBase(WeekDurationsBase, abc.ABC):
     @property
     @abc.abstractmethod
     def average_weekly_time_in_bed(self: Self) -> time: ...
+
+    @computed_field(  # type: ignore[misc]
+        title="Среднее время без сна за неделю",
+    )
+    @property
+    @abc.abstractmethod
+    def average_weekly_no_sleep_time(self: Self) -> time: ...
 
     @computed_field(  # type: ignore[misc]
         title="Средняя эффективность сна (%) за неделю",
