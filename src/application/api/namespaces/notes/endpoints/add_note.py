0fe9a269d5d9b3a8490845a94a08e420c30f8374
Ivan <petyk98@gmail.com>
2024-08-29 15:56:29 +0700

    Исправил декоратор DictPayload и переименовал на DictFromJsonPayload; Добавил TODO.

--- src/application/api/namespaces/notes/endpoints/add_note.py
+++ src/application/api/namespaces/notes/endpoints/add_note.py
@@ -8,7 +8,7 @@ from werkzeug import Response
 from werkzeug.exceptions import BadRequest
 
 from src import service_layer
-from src.application.services import DictPayload
+from src.application.services import DictFromJsonPayload
 from src.domain import ErrorNoteAlreadyExist
 from src.domain.note import (
     ErrorNoSleepDuration,
@@ -30,7 +30,7 @@ class AddNoteEndPoint(Resource):
         database = Database(Settings().POSTGRES_DB_URL)
         repo = ORMDiaryRepository(database)
         owner_id = uuid4()
-        payload: dict[str, str] = DictPayload(request.json).payload
+        payload: dict[str, str] = DictFromJsonPayload(request).json
         try:
             service_layer.write(
                 payload["bedtime_date"],
--- src/application/services.py
+++ src/application/services.py
@@ -1,18 +1,15 @@
-from dataclasses import dataclass, field
-from typing import Any
+from dataclasses import dataclass
 from typing_extensions import Self
 
-from flask import RequestBase
+from werkzeug.wrappers import Request
 
 
 @dataclass
-class DictPayload(RequestBase):
-    json_with_any: Any
-    payload: dict = field(
-        default_factory=lambda: {},
-        init=False,
-    )
+class DictFromJsonPayload(Request):
+    request: Request
 
-    def __post_init__(self: Self) -> None:
-        if isinstance(self._json_with_any, dict):
-            self.payload = self._json_with_any
+    @property
+    def json(self: Self) -> dict:
+        if isinstance(self.request.json, dict):
+            return self.request.json
+        return {}
