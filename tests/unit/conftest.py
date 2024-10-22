304ddecec1b0ed33dcfc24b8efc2e093b70cf4ea
Ivan Novozhilov <petyk98@gmail.com>
2024-10-22 16:42:33 +0700

    Перенес фикстуры из tests/conftest в tests/integration/conftest; Удалил tests/conftest.

--- tests/unit/conftest.py
+++ tests/unit/conftest.py
@@ -1,7 +1,49 @@
 from typing_extensions import Self
 
+import pytest
+
+from punq import Container, Scope
+
 from src.domain.values.points import Points
+from src.infrastructure.repository import BaseDiaryRepository
+from src.infrastructure.repository.memory import MemoryDiaryRepository
+from src.project.containers import get_container
+from src.service_layer import Diary
 
 
 class FakePoints(Points):
     def validate(self: Self) -> None: ...
+
+
+def init_dummy_container() -> Container:
+    container = get_container()
+
+    container.register(
+        BaseDiaryRepository,
+        MemoryDiaryRepository,
+        scope=Scope.singleton,
+    )
+    repository: BaseDiaryRepository = container.resolve(BaseDiaryRepository)
+
+    container.register(
+        Diary,
+        factory=lambda: Diary(repository),
+        scope=Scope.singleton,
+    )
+
+    return container
+
+
+@pytest.fixture
+def container() -> Container:
+    return init_dummy_container()
+
+
+@pytest.fixture
+def diary_repository(container: Container) -> BaseDiaryRepository:
+    return container.resolve(BaseDiaryRepository)
+
+
+@pytest.fixture
+def diary(container: Container) -> Diary:
+    return container.resolve(Diary)
