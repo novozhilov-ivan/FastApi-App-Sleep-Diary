90edb22c102c6eb9dd4e15bfedd3e69c324e4570
Ivan Novozhilov <petyk98@gmail.com>
2025-09-14 15:05:38 +0700

    Feature: ADd sign up

--- src/application/ui/handlers/identity.py
+++ src/application/ui/handlers/identity.py
@@ -1,10 +1,17 @@
-from dishka.integrations.fastapi import DishkaSyncRoute, FromDishka
-from fastapi import APIRouter, Depends
-from fastapi.responses import HTMLResponse
+from typing import Annotated
 
+from dishka.integrations.fastapi import DishkaSyncRoute, FromDishka
+from fastapi import APIRouter, Depends, Form, Request, status
+from fastapi.responses import HTMLResponse, RedirectResponse
+
+from src.application.ui.schemas import MakeSignUpSchema
+from src.domain.identity.exceptions import IdentityError
+from src.domain.sleep_diary.exceptions.base import ApplicationError
 from src.infra.application.pages.me import MePage
 from src.infra.application.pages.sign_in import FetchSignInPage
 from src.infra.application.pages.sign_up import FetchSignUpPage
+from src.infra.identity.services.token_auth import TokenAuth
+from src.infra.identity.use_cases.sign_up import SignUp
 from src.project.containers import config
 
 router = APIRouter(
@@ -15,6 +22,7 @@ router = APIRouter(
 
 @router.get(
     path="/me",
+    status_code=status.HTTP_200_OK,
     response_class=HTMLResponse,
     dependencies=[
         Depends(config.authorization_token.jwt_api_key_cookies),
@@ -29,6 +37,7 @@ def me_page(
 @router.get(
     path="/sign-up",
     response_class=HTMLResponse,
+    status_code=status.HTTP_200_OK,
 )
 def fetch_sign_up_page(
     page: FromDishka[FetchSignUpPage],
@@ -36,8 +45,38 @@ def fetch_sign_up_page(
     return page()
 
 
+@router.post(
+    path="/sign-up",
+    status_code=status.HTTP_201_CREATED,
+    response_class=RedirectResponse,
+)
+def make_sign_up_page(
+    request: Request,
+    form: Annotated[MakeSignUpSchema, Form()],
+    token_auth: FromDishka[TokenAuth],
+    sign_up: FromDishka[SignUp],
+) -> RedirectResponse:
+    try:
+        access_token_claims = sign_up(form.to_command())
+    except (ApplicationError, IdentityError) as error:
+        make_sign_up_page_url = request.url_for("make_sign_up_page")
+        return RedirectResponse(
+            url=f"{make_sign_up_page_url}/?error={error.message}",
+            status_code=status.HTTP_302_FOUND,
+        )
+
+    weeks_info_page_url = request.url_for("weeks_info_page")
+    response = RedirectResponse(
+        url=f"{weeks_info_page_url}/?success=Регистрация успешна",
+        status_code=status.HTTP_302_FOUND,
+    )
+
+    return token_auth.set_session(access_token_claims, response)
+
+
 @router.get(
     path="/sign-in",
+    status_code=status.HTTP_200_OK,
     response_class=HTMLResponse,
 )
 def fetch_sign_in_page(
---
+++ src/application/ui/schemas.py
@@ -0,0 +1,27 @@
+from pydantic import BaseModel, model_validator
+
+from src.infra.identity.use_cases.commands import SignUpInputData
+
+
+class MakeSignUpSchema(BaseModel):
+    username: str
+    plain_password_first: str
+    plain_password_second: str
+
+    @model_validator(mode="after")
+    def check_passwords_match(self) -> "MakeSignUpSchema":
+        if self.plain_password_first != self.plain_password_second:
+            raise ValueError("Пароли не совпадают")
+        return self
+
+    def to_command(self) -> SignUpInputData:
+        return SignUpInputData(
+            username=self.username,
+            plain_password_first=self.plain_password_first,
+            plain_password_second=self.plain_password_second,
+        )
+
+
+class MakeSignIn(BaseModel):
+    username: str
+    plain_password: str
