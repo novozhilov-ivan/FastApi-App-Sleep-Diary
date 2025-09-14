bac47ebac8aff5865b2f4c245589669568f719e0
Ivan Novozhilov <petyk98@gmail.com>
2025-09-14 15:21:17 +0700

    Feature: ADd sign in

--- src/application/ui/handlers/identity.py
+++ src/application/ui/handlers/identity.py
@@ -4,13 +4,14 @@ from dishka.integrations.fastapi import DishkaSyncRoute, FromDishka
 from fastapi import APIRouter, Depends, Form, Request, status
 from fastapi.responses import HTMLResponse, RedirectResponse
 
-from src.application.ui.schemas import MakeSignUpSchema
+from src.application.ui.schemas import MakeSignInSchema, MakeSignUpSchema
 from src.domain.identity.exceptions import IdentityError
 from src.domain.sleep_diary.exceptions.base import ApplicationError
 from src.infra.application.pages.me import MePage
 from src.infra.application.pages.sign_in import FetchSignInPage
 from src.infra.application.pages.sign_up import FetchSignUpPage
 from src.infra.identity.services.token_auth import TokenAuth
+from src.infra.identity.use_cases.sign_in import SignIn
 from src.infra.identity.use_cases.sign_up import SignUp
 from src.project.containers import config
 
@@ -59,15 +60,13 @@ def make_sign_up_page(
     try:
         access_token_claims = sign_up(form.to_command())
     except (ApplicationError, IdentityError) as error:
-        make_sign_up_page_url = request.url_for("make_sign_up_page")
         return RedirectResponse(
-            url=f"{make_sign_up_page_url}/?error={error.message}",
+            url=f"{request.url_for('fetch_sign_up_page')}/?error={error.message}",
             status_code=status.HTTP_302_FOUND,
         )
 
-    weeks_info_page_url = request.url_for("weeks_info_page")
     response = RedirectResponse(
-        url=f"{weeks_info_page_url}/?success=Регистрация успешна",
+        url=f"{request.url_for('weeks_info_page')}/?success=Регистрация успешна",
         status_code=status.HTTP_302_FOUND,
     )
 
@@ -83,3 +82,30 @@ def fetch_sign_in_page(
     page: FromDishka[FetchSignInPage],
 ) -> HTMLResponse:
     return page()
+
+
+@router.post(
+    path="/sign-in",
+    status_code=status.HTTP_200_OK,
+    response_class=RedirectResponse,
+)
+def make_sign_in_page(
+    request: Request,
+    form: Annotated[MakeSignInSchema, Form()],
+    sign_in: FromDishka[SignIn],
+    token_auth: FromDishka[TokenAuth],
+) -> RedirectResponse:
+    try:
+        access_token_claims = sign_in(form.to_command())
+    except (ApplicationError, IdentityError) as error:
+        return RedirectResponse(
+            url=f"{request.url_for('fetch_sign_in_page')}/?error={error.message}",
+            status_code=status.HTTP_302_FOUND,
+        )
+
+    response = RedirectResponse(
+        url=(f"{request.url_for('weeks_info_page')}/?success=Успешная аутентификация"),
+        status_code=status.HTTP_302_FOUND,
+    )
+
+    return token_auth.set_session(access_token_claims, response)
